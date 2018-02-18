41247df477b800d66af17d08aa4b0e3c39d0f019
GETSHOU <borisovsf2013@gmail.com>
2018-02-18 19:25:15 +0300

    Redesign select and buttons

--- app/views/File/file.php
+++ app/views/File/file.php
@@ -1,26 +1,24 @@
 <div class="container">
-    <div class="btn">
-        <button class="move" disabled>Переименовать</button>
-        <button class="copy" disabled>Копировать</button>
-        <button class="remove" disabled>Удалить</button>
-    </div>
 
     <div class="starter-template">
         <h1>Файловый менеджер</h1><br>
-        <div class="fl">
-            <select class="anim">
-                <option selected disabled>Вкл/Выкл анимацию</option>
-                <option value="0">Отключить</option>
-                <option value="1">Включить</option>
 
-            </select>
-            <select class="sel">
-                <option selected disabled>Выбирете шрифт</option>
-                <option value="1">Мелкий</option>
-                <option value="2">Средний</option>
-                <option value="3">Крупный</option>
-            </select>
+        <div class="fl">
+            <div class="select-wrap">
+                <select class="sel">
+                    <option selected disabled>Выбирете шрифт</option>
+                    <option value="1">Мелкий</option>
+                    <option value="2">Средний</option>
+                    <option value="3">Крупный</option>
+                </select>
+                <select class="anim">
+                    <option selected disabled>Вкл/Выкл анимацию</option>
+                    <option value="0">Отключить</option>
+                    <option value="1">Включить</option>
+                </select>
+            </div>
         </div>
+
         <div class="box">
             <div class="box-elem">
                 <div class="elem"></div>
@@ -28,5 +26,12 @@
                 <div class="elem-info"></div>
             </div>
         </div>
+
+        <div class="btn">
+            <button class="move" disabled>Переименовать</button>
+            <button class="copy" disabled>Копировать</button>
+            <button class="remove" disabled>Удалить</button>
+        </div>
+
     </div>
 </div>
